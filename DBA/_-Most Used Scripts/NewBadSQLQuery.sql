931ad341c4316873d3b6ed3fd999ceed37be43e8
Greylancer1 <75636971+Greylancer1@users.noreply.github.com>
2021-01-04 17:06:03 -0800

    Change Bad SQL Query to look for logical reads

--- DBA/_-Most Used Scripts/NewBadSQLQuery.sql
+++ DBA/_-Most Used Scripts/NewBadSQLQuery.sql
@@ -15,7 +15,7 @@ CROSS APPLY sys.dm_exec_sql_text(a.sql_handle) st
 WHERE
 DB_NAME(st.dbid) NOT IN ('master','msdb','ReportServer','ReportServerTempDB')
 ORDER BY 
-total_elapsed_time/execution_count DESC --Longest Running
---total_logical_reads/execution_count DESC --Most Logical Reads
+--total_elapsed_time/execution_count DESC --Longest Running
+total_logical_reads/execution_count DESC --Most Logical Reads
 --total_physical_reads / execution_count DESC --Most Physical Reads
 --(total_worker_time/execution_count)/1000 DESC --Most CPU
